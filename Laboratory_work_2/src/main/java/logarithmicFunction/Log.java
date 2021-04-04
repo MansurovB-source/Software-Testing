4b52e1ec8419bdd8e7589d444ed6e9f4e1b0a708
Behruz Mansurov <bekha.m2000.bm@gmail.com>
2021-04-04 13:06:33 +0300

    Updated

--- Laboratory_work_2/src/main/java/logarithmicFunction/Log.java
+++ Laboratory_work_2/src/main/java/logarithmicFunction/Log.java
@@ -18,6 +18,16 @@ public class Log implements LogarithmicFunction {
 
     @Override
     public double apply(double value, int base) {
+        if (!LogarithmicFunction.isValid(value)) {
+            if (Double.isNaN(value) || value < 0.0) {
+                return Double.NaN;
+            } else if (Double.compare(Double.POSITIVE_INFINITY, value) == 0) {
+                return Double.POSITIVE_INFINITY;
+            } else {
+                return Double.NEGATIVE_INFINITY;
+            }
+        }
+
         if (!LogarithmicFunction.isValidBase(base)) {
             throw new IllegalArgumentException("Unsupportable base");
         }
